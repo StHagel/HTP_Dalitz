f62a94f71035584fa8f98d3ca32098e475f97523
sthagel <sthagel@uw.edu>
2020-06-02 22:51:48 +0200

    Added basic csv reading backbone
    The code now reads the contents of the momenta.csv file and calculates
    the corresponding four-vectors from it.

--- src/main.py
+++ src/main.py
@@ -21,9 +21,28 @@ import numpy as np
 import csv
 
 if __name__ == '__main__':
+    # Since the momenta are all given in units of GeV, the masses of the three particles are also in GeV
+    m_pi_c = 0.13957018
+    m_k_c = 0.493677
+    m_pi_0 = 0.1349766
     with open("../data/momenta.csv") as csv_file:
         momentum_reader = csv.reader(csv_file)
-        pass
 
+        # These variables contain the four-momenta of the decay products
+        p4_pi_c = []
+        p4_k_c = []
+        p4_pi_0 = []
 
+        for row in momentum_reader:
+            e_pi_c = np.sqrt(m_pi_c ** 2 + float(row[0]) ** 2 + float(row[1]) ** 2 + float(row[2]) ** 2)
+            p4_pi_c.append([e_pi_c, row[0], row[1], row[2]])
 
+            e_k_c = np.sqrt(m_k_c ** 2 + float(row[3]) ** 2 + float(row[4]) ** 2 + float(row[5]) ** 2)
+            p4_k_c.append([e_k_c, row[3], row[4], row[5]])
+
+            e_pi_0 = np.sqrt(m_pi_0 ** 2 + float(row[6]) ** 2 + float(row[7]) ** 2 + float(row[8]) ** 2)
+            p4_pi_0.append([e_pi_0, row[6], row[7], row[8]])
+
+        print(p4_pi_c[0])
+        print(p4_k_c[0])
+        print(p4_pi_0[0])
