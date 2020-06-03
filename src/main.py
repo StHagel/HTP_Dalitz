4df890129f08bbb35b6d33bce46787a71176bfe3
sthagel <sthagel@uw.edu>
2020-06-03 12:16:22 +0200

    Added Dalitz plot and mother mass functionality
    The code now has a function to draw the Dalitz plot for each pair of
    invariant masses. Also added a function to calculate the mass of the
    mother particle and plot it.

--- src/main.py
+++ src/main.py
@@ -20,6 +20,69 @@
 import numpy as np
 import math
 import csv
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+import statistics as stat
+
+
+def invariant_mass(p1, p2):
+    """
+    Given two four-momenta, this function calculates the invariant mass squared, i.e. the Mandlestam variable s.
+    :param p1: The first four-momentum (list of length 4).
+    :param p2: The second four-momentum (list of length 4).
+    :return: The invariant mass squared (float).
+    """
+    return (p1[0] + p2[0]) ** 2 - ((p1[1] + p2[1]) ** 2 + (p1[2] + p2[2]) ** 2 + (p1[3] + p2[3]) ** 2)
+
+
+def mother_particle_mass(p1, p2, p3):
+    """
+    Calculates the mean total energy of three particles and the variance.
+    From this we can get the mother particle's mass.
+    :param p1: List of four momenta of particle 1.
+    :param p2: List of four momenta of particle 2.
+    :param p3: List of four momenta of particle 3.
+    :return: A list of mother particle masses.
+    """
+
+    total_energies = []
+
+    for j, entry in enumerate(p1):
+        energy = entry[0] + p2[j][0] + p3[j][0]
+        total_energies.append(energy)
+
+    return total_energies
+
+
+def draw_dalitz_plot(_df):
+    """
+    Draws a Dalitz plot from two given sets of invariant masses.
+    :param _df: A pandas dataframe containing the invariant masses of the decay products.
+    :return:
+    """
+
+    sns.pairplot(df, corner=True, diag_kind="kde", markers="+")
+    plt.show()
+
+
+def plot_mass(data):
+    mean = stat.mean(data)
+    sigma = stat.stdev(data)
+
+    plt.figure()
+    plt.plot(data, "x", markersize=2)
+    plt.title("Mother particle mass")
+    plt.xlabel("Event number")
+    plt.ylabel(r"$m$[GeV]")
+    plt.grid(True)
+    plt.plot([mean for k in range(len(data))], "red",
+             label=r"$m=(" + "%.4f" % mean + "\pm" + "%.4f" % sigma + ")\;$GeV")
+    plt.legend()
+    plt.plot([(mean + sigma) for k in range(len(data))], "r:",)
+    plt.plot([(mean - sigma) for k in range(len(data))], "r:",)
+    plt.show()
+
 
 if __name__ == '__main__':
     # Since the momenta are all given in units of GeV, the masses of the three particles are also in GeV
@@ -34,17 +97,36 @@ if __name__ == '__main__':
         p4_k_c = []
         p4_pi_0 = []
 
-        for row in momentum_reader:
+        # These variables hold the actual invariant masses
+        m_pi_c_k_c = []
+        m_pi_c_pi_0 = []
+        m_k_c_pi_0 = []
+
+        for i, row in enumerate(momentum_reader):
             # The energies are calculated using the standard relativistic dispersion relation
             e_pi_c = math.sqrt(m_pi_c ** 2 + float(row[0]) ** 2 + float(row[1]) ** 2 + float(row[2]) ** 2)
-            p4_pi_c.append([e_pi_c, row[0], row[1], row[2]])
+            p4_pi_c.append([e_pi_c, float(row[0]), float(row[1]), float(row[2])])
 
             e_k_c = math.sqrt(m_k_c ** 2 + float(row[3]) ** 2 + float(row[4]) ** 2 + float(row[5]) ** 2)
-            p4_k_c.append([e_k_c, row[3], row[4], row[5]])
+            p4_k_c.append([e_k_c, float(row[3]), float(row[4]), float(row[5])])
 
             e_pi_0 = math.sqrt(m_pi_0 ** 2 + float(row[6]) ** 2 + float(row[7]) ** 2 + float(row[8]) ** 2)
-            p4_pi_0.append([e_pi_0, row[6], row[7], row[8]])
+            p4_pi_0.append([e_pi_0, float(row[6]), float(row[7]), float(row[8])])
 
-        print(p4_pi_c[0])
-        print(p4_k_c[0])
-        print(p4_pi_0[0])
+            # The invariant masses are calculated using the invariant_mass function defined above
+            m_pi_c_k_c.append(invariant_mass(p4_pi_c[i], p4_k_c[i]))
+            m_pi_c_pi_0.append(invariant_mass(p4_pi_c[i], p4_pi_0[i]))
+            m_k_c_pi_0.append(invariant_mass(p4_k_c[i], p4_pi_0[i]))
+
+        masses = {
+            "m12": m_pi_c_k_c,
+            "m23": m_k_c_pi_0,
+            "m13": m_pi_c_pi_0
+        }
+        df = pd.DataFrame(masses)
+
+        draw_dalitz_plot(df)
+
+        m_mother_list = mother_particle_mass(p4_pi_c, p4_k_c, p4_pi_0)
+
+        plot_mass(m_mother_list)
